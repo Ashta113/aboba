13e29770599fe58d5f5f8b1ea168253688d374dd
Алдар <142901513+Ashta113@users.noreply.github.com>
2025-02-08 17:40:24 +0300

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,59 @@
+def prod_non_zero_diag(x):
+    n = 1
+    for i in range(min(len(x), len(x[0]))):
+        if (x[i][i] != 0):
+            n *= x[i][i]
+    return n
+
+
+def are_multisets_equal(x, y):
+    s1 = set(x)
+    s2 = set(y)
+    return s1 == s2
+
+
+def max_after_zero(x):
+    pass
+
+
+def convert_image(img, coefs):
+    """Sum up image channels with weights from coefs array
+
+    input:
+    img -- 3-d numpy array (H x W x 3)
+    coefs -- 1-d numpy array (length 3)
+    output:
+    img -- 2-d numpy array
+
+    Not vectorized implementation.
+    """
+
+    pass
+
+
+def run_length_encoding(x):
+    """Make run-length encoding.
+
+    input:
+    x -- 1-d numpy array
+    output:
+    elements, counters -- integer iterables
+
+    Not vectorized implementation.
+    """
+
+    pass
+
+
+def pairwise_distance(x, y):
+    """Return pairwise object distance.
+
+    input:
+    x, y -- 2d numpy arrays
+    output:
+    distance array -- 2d numpy array
+
+    Not vectorized implementation.
+    """
+
+    pass
